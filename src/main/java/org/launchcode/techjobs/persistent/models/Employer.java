aeaff651a709480b47bb6a9a1f574896101fcb30
Grant Crawford <grantttt.crawford@gmail.com>
2023-03-16 19:10:08 -0500

    part 1 and some of part 2

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,24 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotBlank
+    @Size(min = 3, max = 33, message = "Minimum length 3 max 33 not blank")
+    private String location;
 
+    public Employer() {
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
